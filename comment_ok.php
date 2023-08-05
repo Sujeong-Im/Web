67505717a5309be36930bef3b532685b3884403f
root <root@Eve.vjfgimzjadnuphaacar2ymlhif.syx.internal.cloudapp.net>
2023-08-05 15:33:59 +0000

    EDIT COMMENT

--- comment_ok.php
+++ comment_ok.php
@@ -6,12 +6,8 @@ $number = $_POST['board_number'];
 $content = $_POST['comment_content'];
 $date = date('Y-m-d H:i:s');
 
-$query = "INSERT INTO comment (board_number, id, content, date, comment_number) VALUES (?, ?, ?, ?, null)";
-
-$stmt = $connect->prepare($query);
-$stmt->bind_param("isss", $number, $id, $content, $date);
-
-$result = $stmt->execute();
+$query = "INSERT INTO comment (board_number, id, content, date, comment_number) VALUES ('$number', '$id', '$content', '$date', null)";
+$result = $connect->query($query);
 
 if ($result) {
     ?>
@@ -21,10 +17,9 @@ if ($result) {
     </script>
     <?php
 } else {
-    // 쿼리 오류 메시지 출력
     echo "Failed : " . mysqli_error($connect);
 }
 
-$stmt->close();
 mysqli_close($connect);
 ?>
+
